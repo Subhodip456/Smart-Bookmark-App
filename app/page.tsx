e25ce1bd0fad8301f12710229e80ec5e84b4def0
Subhodip Datta <53405787+Subhodip456@users.noreply.github.com>
2026-02-19 01:02:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "https://smart-bookmark-app-woad-psi.vercel.app/auth/callback",
+        redirectTo: "https://smart-bookmark-app-woad-psi.vercel.app/dashboard",
       },
     });
   };
