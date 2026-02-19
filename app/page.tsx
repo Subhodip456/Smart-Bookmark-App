bdc98660616e1011d53b3b685dc27eba4b39227f
Subhodip Datta <53405787+Subhodip456@users.noreply.github.com>
2026-02-19 23:08:34 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "https://ndczwufytdgwumpxotqp.supabase.co/auth/v1/callback",
+        redirectTo: "https://ndczwufytdgwumpxotqp.supabase.co/dashboard",
       },
     });
   };
